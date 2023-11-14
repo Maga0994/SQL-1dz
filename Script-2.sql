65d770447812023af0699d66b35799b417a19a35
Gadzhaev Magomet <gadzhaev.magomet@gmail.com>
2023-11-14 05:06:28 +0300

    change code

--- Script-2.sql
+++ Script-2.sql
@@ -1,52 +1,51 @@
 CREATE TABLE IF NOT EXISTS Albums (
-   album_id SERIAL(40) PRIMARY KEY,
+   album_id SERIAL PRIMARY KEY,
    name varchar(40) NOT NULL,
    release_year VARCHAR(15) NOT NULL
 );
 
 
 CREATE TABLE IF NOT EXISTS Sounds (
-    sound_id SERIAL(40) PRIMARY KEY,
-    album_id VARCHAR(40) NOT NULL REFERENCES Albums(album_id),
+    sound_id SERIAL PRIMARY KEY,
+    album_id SERIAL NOT NULL REFERENCES Albums(album_id),
     name VARCHAR(40) NOT NULL,
-    time INTEGER NOT NULL
+    time INTERVAL NOT NULL
 );
 
 
 CREATE TABLE IF NOT EXISTS Collections (
-    collection_id SERIAL(40) PRIMARY KEY,
+    collection_id SERIAL PRIMARY KEY,
     name VARCHAR(40) NOT NULL,
     release_date DATE NOT NULL
 );
 
 
 CREATE TABLE IF NOT EXISTS Executors (
-    executor_id SERIAL(40) PRIMARY KEY,
+    executor_id SERIAL PRIMARY KEY,
     nickname VARCHAR(40) UNIQUE NOT NULL
 );
 
 
 CREATE TABLE IF NOT EXISTS Genres (
-    genre_id SERIAL(40) PRIMARY KEY,
+    genre_id SERIAL PRIMARY KEY,
     name VARCHAR(40) NOT NULL
 );
 
 
 CREATE TABLE IF NOT EXISTS SoundsCollections (
-    id SERIAL(40) PRIMARY KEY,
-    sound_id VARCHAR(40) NOT NULL REFERENCES Collections(collection_id),
-    collection_id VARCHAR(40) NOT NULL REFERENCES Sounds(sound_id)
+    id SERIAL PRIMARY KEY,
+    sound_id SERIAL NOT NULL REFERENCES Sounds(sound_id),
+    collection_id SERIAL NOT NULL REFERENCES Collections(collection_id)
 );
 
-
 CREATE TABLE IF NOT EXISTS GenresExecutors (
-    id SERIAL(40) PRIMARY KEY,
-    genre_id VARCHAR(40) NOT NULL REFERENCES Executors(executors_id),
-    executor_id VARCHAR(40) NOT NULL REFERENCES Genres(genre_id)
+    id SERIAL PRIMARY KEY,
+    genre_id SERIAL NOT NULL REFERENCES Genres(genre_id),
+    executor_id SERIAL NOT NULL REFERENCES Executors(executor_id)
 );
 
 CREATE TABLE IF NOT EXISTS AlbumsExecutors (
-    id SERIAL(40) PRIMARY KEY,
-    executor_id VARCHAR(40) NOT NULL REFERENCES Albums(album_id),
-    album_id VARCHAR(40) NOT NULL REFERENCES Executors(executors_id)
+    id SERIAL PRIMARY KEY,
+    album_id SERIAL NOT NULL REFERENCES Albums(album_id),
+    executor_id SERIAL NOT NULL REFERENCES Executors(executor_id)
 );
